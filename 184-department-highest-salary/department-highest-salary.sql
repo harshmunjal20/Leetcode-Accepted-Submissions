2388e2e033d70efd7e8293bf38d991ad035ec1c8
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-16 14:24:28 +0530

    Time: 731 ms (61.55%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,6 +1,5 @@
--- # Write your MySQL query statement below
--- WITH CTE AS (SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary FROM Employee e INNER JOIN Department d ON e.departmentId = d.id)
-
--- SELECT Department, Employee, MAX(Salary) AS Salary FROM CTE GROUP BY Department;
-SELECT d.name AS Department, e.name AS Employee, temp.max_salary AS Salary FROM Employee e INNER JOIN Department d INNER JOIN (SELECT MAX(Salary) AS max_salary, departmentId FROM Employee GROUP BY departmentId) AS Temp ON e.departmentId = d.id AND d.id = temp.departmentId WHERE e.salary = temp.max_salary;
-
+# Write your MySQL query statement below
+SELECT d.name AS Department, e.name AS Employee, Temp.Salary FROM Employee e
+INNER JOIN Department d INNER JOIN
+(SELECT departmentId, MAX(Salary) AS salary FROM Employee GROUP BY departmentId) AS Temp
+ON e.departmentId = d.id AND d.id = temp.departmentId AND e.salary = temp.salary;

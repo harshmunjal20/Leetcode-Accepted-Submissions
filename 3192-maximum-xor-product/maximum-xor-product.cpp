4de6a04e2387465b1accc1acf9e9f2b32fd6d0aa
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-18 18:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (23.79%) - LeetSync

---
+++ 3192-maximum-xor-product/maximum-xor-product.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumXorProduct(long long a, long long b, int n) {
+        const long long MOD = 1e9 + 7;
+
+        for (int i = n - 1; i >= 0; i--) {
+            long long bit = 1LL << i;
+
+            long long firstNum = a ^ bit;
+            long long secondNum = b ^ bit;
+
+            __int128 firstProd = (__int128)firstNum * secondNum;
+            __int128 secondProd = (__int128)a * b;
+
+            if (firstProd > secondProd) {
+                a ^= bit;
+                b ^= bit;
+            }
+        }
+
+        return ((__int128)a * b) % MOD;
+    }
+};

d0b7def1fbe0fcf0c68939b0ddb7a89fdd85cbd6
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-22 10:18:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.35%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = int(math.sqrt(n))
+        return count
+
+        

2b021b746cf6a6591f7bea5f8d173786e947d428
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-22 22:25:18 +0530

    Time: 152 ms (27.46%) | Memory: 14.5 MB (97.89%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.py
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.py
@@ -9,25 +9,23 @@ class Solution(object):
         :type maxSize: int
         :rtype: int
         """
-        freqMap = defaultdict(int)
-        strFreqMap = defaultdict(int)
+        freqIdxMap = defaultdict(int)
+        strOccurences = defaultdict(int)
+
         i , j = 0, 0
 
         while j < len(s):
-            freqMap[s[j]] += 1
+            freqIdxMap[s[j]] += 1
 
-            while len(freqMap) > maxLetters or (j - i + 1) > minSize:
-                freqMap[s[i]] -= 1
-                if freqMap[s[i]] == 0:
-                    freqMap.pop(s[i])
+            while (j - i + 1) > minSize or len(freqIdxMap) > maxLetters:
+                freqIdxMap[s[i]] -= 1
+                if (freqIdxMap[s[i]] == 0):
+                    freqIdxMap.pop(s[i])
                 i += 1
-
-            if len(freqMap) <= maxLetters and (j - i + 1) >= minSize:
-                strFreqMap[s[i:j + 1]] += 1
-
+            
+            if (j - i + 1) == minSize and len(freqIdxMap) <= maxLetters:
+                strOccurences[s[i : j + 1]] += 1
+            
             j += 1
-
-        return max(strFreqMap.values()) if strFreqMap else 0
-
-
-              
+        
+        return max(strOccurences.values()) if strOccurences else 0

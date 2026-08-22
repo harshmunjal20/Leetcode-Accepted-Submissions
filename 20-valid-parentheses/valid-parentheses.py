b3254a558122dddd5f38b0f0ee0c9e91a91dacb5
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-22 05:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (39.29%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -5,14 +5,13 @@ class Solution(object):
         :rtype: bool
         """
         stack = []
+        pairs = {')' : '(', ']' : '[', '}' : '{'}
 
-        for ch in s:
-            if (stack and ((ch == ')' and stack[-1] == '(') or (ch == '}' and stack[-1] == '{') or (ch == ']' and stack[-1] == '['))):
-                stack.pop()
-            elif (stack and ((ch == ')' and stack[-1] != '(') or (ch == '}' and stack[-1] != '{') or (ch == ']' and stack[-1] != '['))):
-                return False
+        for char in s:
+            if char in pairs:
+                if not stack or stack.pop() != pairs[char]:
+                    return False
             else:
-                stack.append(ch)
-
-        return len(stack) == 0
+                stack.append(char)
         
+        return not stack

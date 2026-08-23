9b251fe14936fc19144a70c6ec74a98b20ee7dd0
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-23 20:19:43 +0530

    Time: 503 ms (90.51%) | Memory: 35.4 MB (92.09%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,6 +1,7 @@
 import heapq
 
 class MedianFinder(object):
+
     def __init__(self):
         self.min_heap = []
         self.max_heap = []
@@ -10,27 +11,30 @@ class MedianFinder(object):
         :type num: int
         :rtype: None
         """
-        if len(self.max_heap) <= len(self.min_heap): # push in maxHeap the num
-            if not self.max_heap or (self.max_heap and -self.max_heap[0] >= num):
+        if len(self.min_heap) >= len(self.max_heap): # push it in the maxHeap
+            if not self.max_heap or num < -self.max_heap[0]:
                 heapq.heappush(self.max_heap, -num)
-            elif self.min_heap:
+            else:
+                # minHeap to maxHeap transfer
                 heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
                 heapq.heappush(self.min_heap, num)
-        else: # push in min_heap the num
-            if (self.max_heap and -self.max_heap[0] <= num):
+        else: # push it in the minHeap
+            if num >= -self.max_heap[0]:
                 heapq.heappush(self.min_heap, num)
-            elif self.max_heap:
+            else:
+                # tranfer top of maxheap to minheap and num to maxheap
                 heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
                 heapq.heappush(self.max_heap, -num)
+        
 
     def findMedian(self):
         """
         :rtype: float
         """
         if (len(self.min_heap) + len(self.max_heap)) % 2 == 0:
-            return (-self.max_heap[0] + self.min_heap[0]) / 2.0
+            return (self.min_heap[0] - self.max_heap[0]) / 2.0
         return min(-self.max_heap[0] if self.max_heap else 1e9, self.min_heap[0] if self.min_heap else 1e9)
-
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:

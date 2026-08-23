7a245653b7d5ee0b5bb69951eede0ade1d19ad4b
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-23 15:39:24 +0530

    Time: 81 ms (86.31%) | Memory: 17.3 MB (5.95%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.py
+++ 526-beautiful-arrangement/beautiful-arrangement.py
@@ -1,19 +1,26 @@
-from functools import lru_cache
-
-class Solution:
-    def countArrangement(self, n: int) -> int:
-        @lru_cache
-        def permutation(idx, bitMask):
+class Solution(object):
+    def countArrangement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def permutation(idx, bitMask, dp):
             if idx > n:
                 return 1
+            
+            if dp[idx][bitMask] != -1:
+                return dp[idx][bitMask]
 
             numWays = 0
 
             for i in range(1, n + 1):
                 if not bitMask & (1 << (i - 1)):
-                    if i % idx == 0 or idx % i == 0:
-                        numWays += permutation(idx + 1, bitMask | (1 << (i - 1)))
-
+                    if idx % i == 0 or i % idx == 0:
+                        numWays += permutation(idx + 1, bitMask | (1 << (i - 1)), dp)
+            
+            dp[idx][bitMask] = numWays
             return numWays
-
-        return permutation(1, 0)
+        
+        dp = [[-1 for _ in range(1 << n)] for _ in range(n + 1)]
+        return permutation(1, 0, dp)
+        

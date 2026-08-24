47586c6992e84e8ea191b7188ec8ba67c845b884
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-24 13:40:36 +0530

    Time: 2409 ms (33.33%) | Memory: 13.8 MB (77.78%) - LeetSync

---
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def countPairsOfConnectableServers(self, edges, signalSpeed):
+        """
+        :type edges: List[List[int]]
+        :type signalSpeed: int
+        :rtype: List[int]
+        """
+        def DFS(node, weight, parent, adj):
+            countNodes = 0
+
+            for neighbourNode , neighbourWeight in adj[node]:
+                if neighbourNode != parent:
+                    if (weight + neighbourWeight) % signalSpeed == 0:
+                        countNodes += 1
+                    countNodes += DFS(neighbourNode, weight + neighbourWeight, node, adj)
+            
+            return countNodes
+                    
+
+        nodesCount = len(edges) + 1
+        count = [0] * nodesCount
+        adj = [[] for _ in range(nodesCount)]
+
+        for u, v, w in edges:
+            adj[u].append((v, w))
+            adj[v].append((u, w))
+
+        for node in range(nodesCount):
+            parent = node
+            connectableNodes = 0
+
+            for neighbourNode , weight in adj[node]:
+                currCount = 0
+                currCount += DFS(neighbourNode, weight, parent, adj)
+                if weight % signalSpeed == 0:
+                    currCount += 1
+                
+                count[node] += currCount * connectableNodes
+                connectableNodes += currCount
+
+        return count
+
+
+        

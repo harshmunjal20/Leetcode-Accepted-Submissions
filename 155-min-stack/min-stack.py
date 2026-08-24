3c4239640b238a0e47a25cad0a4d966e3c6c1852
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-24 10:46:12 +0530

    Time: 109 ms (98.60%) | Memory: 24.4 MB (86.63%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,22 +2,28 @@ class MinStack(object):
 
     def __init__(self):
         self.stack = []
+        self.currMin = None
 
     def push(self, value):
         """
         :type value: int
         :rtype: None
         """
-        if self.stack:
-            self.stack.append((value, min(value, self.stack[-1][1])))
+        if not self.stack:
+            self.stack.append(value)
+            self.currMin = value
+        elif value < self.currMin:
+            self.stack.append(2 * value - self.currMin)
+            self.currMin = value
         else:
-            self.stack.append((value, value))
-        
+            self.stack.append(value)
 
     def pop(self):
         """
         :rtype: None
         """
+        if self.stack[-1] < self.currMin:
+            self.currMin = 2 * self.currMin - self.stack[-1]
         self.stack.pop()
         
 
@@ -25,14 +31,15 @@ class MinStack(object):
         """
         :rtype: int
         """
-        return self.stack[-1][0]
-        
+        if self.stack[-1] < self.currMin:
+            return self.currMin
+        return self.stack[-1]
 
     def getMin(self):
         """
         :rtype: int
         """
-        return self.stack[-1][1]
+        return self.currMin
         
 
 

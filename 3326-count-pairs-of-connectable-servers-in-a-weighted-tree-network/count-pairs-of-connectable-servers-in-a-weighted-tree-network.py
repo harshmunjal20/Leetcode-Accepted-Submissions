d5f01073554a4b0ef219148f9ff87eb7d150f3c3
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-24 13:57:14 +0530

    Time: 2346 ms (44.44%) | Memory: 13.7 MB (77.78%) - LeetSync

--- 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
@@ -5,36 +5,33 @@ class Solution(object):
         :type signalSpeed: int
         :rtype: List[int]
         """
-        def DFS(node, weight, parent, adj):
-            countNodes = 0
+        def DFS(node, parent, weight, adj):
+            nodes = 0
+
             if weight % signalSpeed == 0:
-                countNodes += 1
+                nodes += 1
 
-            for neighbourNode , neighbourWeight in adj[node]:
-                if neighbourNode != parent:
-                    countNodes += DFS(neighbourNode, weight + neighbourWeight, node, adj)
-            
-            return countNodes
-                    
+            for neighbour, neighbourWeight in adj[node]:
+                if neighbour != parent:
+                    nodes += DFS(neighbour, node, weight + neighbourWeight, adj)
 
-        nodesCount = len(edges) + 1
-        count = [0] * nodesCount
-        adj = [[] for _ in range(nodesCount)]
+            return nodes
+
+        countNodes = len(edges) + 1
+        adj = [[] for _ in range(countNodes)]
+        count = [0] * countNodes
 
         for u, v, w in edges:
             adj[u].append((v, w))
             adj[v].append((u, w))
 
-        for node in range(nodesCount):
+        for node in range(countNodes):
+            nodesSeen = 0
             parent = node
-            connectableNodes = 0
-
-            for neighbourNode , weight in adj[node]:
-                currCount = DFS(neighbourNode, weight, parent, adj)
-                count[node] += currCount * connectableNodes
-                connectableNodes += currCount
-
+            
+            for neighbour, weight in adj[node]:
+                currBranchNodes = DFS(neighbour, parent, weight, adj)
+                count[node] += currBranchNodes * nodesSeen
+                nodesSeen += currBranchNodes
+    
         return count
-
-
-        

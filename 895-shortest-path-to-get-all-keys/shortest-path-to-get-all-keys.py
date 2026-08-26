b16420fb121d3cdf1bf18294030ffb9a80f43682
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-26 18:21:53 +0530

    Time: 241 ms (42.53%) | Memory: 18 MB (14.94%) - LeetSync

--- 895-shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.py
+++ 895-shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.py
@@ -1,61 +1,55 @@
-from collections import deque
-
 class Solution(object):
-    
     def shortestPathAllKeys(self, grid):
         """
         :type grid: List[str]
         :rtype: int
         """
-        xDir = [-1, 0, 1, 0]
-        yDir = [0, 1, 0, -1]
         rows = len(grid)
         cols = len(grid[0])
+        directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
         queue = deque()
-        minMoves = 0
-        totalKeys = 0
         visited = set()
+        totalKeys = 0
 
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == '@':
-                    queue.append((i * cols + j, 0)) # third argument is keys
+                    queue.append((i * cols + j, 0))
                     visited.add((i * cols + j, 0))
-                elif grid[i][j] >= 'a' and grid[i][j] <= 'z':
+                elif 'a' <= grid[i][j] <= 'f':
                     totalKeys += 1
-
-        minMoves = 0
+        
+        target = (1 << totalKeys) - 1
+        totalMoves = 0
 
         while queue:
             sz = len(queue)
 
             while sz > 0:
-                decodedNum, keys = queue.popleft()
+                decodedNum , keys = queue.popleft()
                 i = decodedNum / cols
                 j = decodedNum % cols
 
-                target_mask = (1 << totalKeys) - 1
+                if keys == target:
+                    return totalMoves
 
-                if target_mask == keys:
-                    return minMoves
+                for dirX, dirY in directions:
+                    x = dirX + i
+                    y = dirY + j
 
-                for index in range(4):
-                    x = xDir[index] + i
-                    y = yDir[index] + j
-
-                    if x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0]) and grid[x][y] != '#' and ((x * cols + y, keys) not in visited):
+                    if x >= 0 and x < rows and y >= 0 and y < cols and grid[x][y] != '#' and not (x * cols + y, keys) in visited:
                         numKeys = keys
 
-                        if grid[x][y] >= 'A' and grid[x][y] <= 'Z' and (keys & (1 << (ord(grid[x][y]) - ord('A')))) == 0:
+                        if 'A' <= grid[x][y] <= 'F' and not (numKeys & (1 << (ord(grid[x][y]) - ord('A')))):
                             continue
-                        elif grid[x][y] >= 'a' and grid[x][y] <= 'z':
+                        elif 'a' <= grid[x][y] <= 'f' :
                             numKeys = numKeys | (1 << (ord(grid[x][y]) - ord('a')))
-                        
-                        visited.add((x * cols + y, numKeys))
+
+                        visited.add((x * cols +  y, numKeys))
                         queue.append((x * cols + y, numKeys))
-                
+
                 sz -= 1
 
-            minMoves += 1
-
+            totalMoves += 1
+        
         return -1

5e74ee503e7f0ed4864e95cf610a0fe926d45e83
Harsh Munjal <sarlamunjal4@gmail.com>
2026-08-26 19:07:59 +0530

    Time: 616 ms (57.23%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT e.employee_id FROM Employees e LEFT JOIN Salaries s ON e.employee_id = s.employee_id WHERE e.name IS NULL OR s.salary IS NULL
+SELECT e.employee_id FROM Employees e LEFT JOIN Salaries s ON e.employee_id = s.employee_id WHERE s.salary IS NULL
 UNION
-SELECT s.employee_id FROM Salaries s LEFT JOIN Employees e ON e.employee_id = s.employee_id WHERE e.name IS NULL OR s.salary IS NULL
+SELECT s.employee_id FROM Salaries s LEFT JOIN Employees e ON e.employee_id = s.employee_id WHERE e.name IS NULL
 ORDER BY employee_id

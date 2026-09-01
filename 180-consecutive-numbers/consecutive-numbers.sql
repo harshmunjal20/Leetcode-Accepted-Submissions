ced4cf4894feaa5295f6f542feb25a343b5764e1
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-01 21:09:46 +0530

    Time: 632 ms (45.44%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,14 +1,13 @@
 # Write your MySQL query statement below
 # using window function LEAD(num, offset)
 
-WITH consecutiveNumsTable AS (
-    SELECT id, num, 
-    LEAD(num, 1) OVER() AS next_Day_num,
-    LEAD(num, 2) OVER() AS next_next_Day_num
+WITH ConsecutiveNumsTable AS (
+    SELECT num, 
+    LEAD(num, 1) OVER() AS next_day_num,
+    LEAD(num, 2) OVER() AS next_next_day_num
     FROM Logs
 )
 
-SELECT DISTINCT num
-AS ConsecutiveNums
-FROM consecutiveNumsTable
-WHERE num = next_day_num AND num = next_next_Day_num
+SELECT DISTINCT num AS ConsecutiveNums
+FROM ConsecutiveNumsTable
+WHERE num = next_day_num AND num = next_next_day_num

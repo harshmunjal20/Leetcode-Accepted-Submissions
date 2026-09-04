48159a09b186441ab5921c2917511aec04a5a557
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-04 22:07:14 +0530

    Time: 1437 ms (58.62%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,19 +1,21 @@
 # Write your MySQL query statement below
-(SELECT u.name AS results
-FROM Users u
-JOIN MovieRating mr
-    ON u.user_id = mr.user_id
+(SELECT u.name AS results 
+FROM Users u 
+INNER JOIN MovieRating mr
+ON u.user_id = mr.user_id
 GROUP BY u.user_id
 ORDER BY COUNT(mr.rating) DESC, u.name ASC
-LIMIT 1)
+LIMIT 1
+)
 
 UNION ALL
 
 (SELECT m.title AS results
 FROM Movies m
-JOIN MovieRating mr
-    ON m.movie_id = mr.movie_id
-WHERE mr.created_at LIKE "2020-02-%"
+INNER JOIN MovieRating mr
+ON m.movie_id = mr.movie_id
+WHERE created_at LIKE "2020-02-%"
 GROUP BY m.movie_id
 ORDER BY AVG(mr.rating) DESC, m.title ASC
-LIMIT 1);
+LIMIT 1
+)

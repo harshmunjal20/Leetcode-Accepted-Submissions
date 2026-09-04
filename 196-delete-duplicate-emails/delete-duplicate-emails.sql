06aec16239d34830adde07408599cd112277df99
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-04 14:44:09 +0530

    Time: 1130 ms (12.41%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,7 +1,4 @@
-DELETE FROM Person WHERE id NOT IN (
-    SELECT min_id FROM (
-        SELECT MIN(id) AS min_id
-        FROM Person
-        GROUP BY email
-    ) AS Temp
-)
+DELETE p1 
+FROM Person p1
+JOIN Person p2
+WHERE p1.email = p2.email AND p1.id > p2.id

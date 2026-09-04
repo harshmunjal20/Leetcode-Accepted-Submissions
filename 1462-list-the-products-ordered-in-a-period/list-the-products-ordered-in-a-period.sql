d2d332bb1cceb18dc8e74cd636b1164d8706405a
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-04 17:10:49 +0530

    Time: 868 ms (37.93%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -2,6 +2,6 @@
 SELECT p.product_name, SUM(o.unit) AS unit
 FROM Products p
 INNER JOIN Orders o
-ON p.product_id = o.product_id AND order_date BETWEEN "2020-02-01" AND "2020-02-29"
+ON p.product_id = o.product_id AND order_date LIKE "2020-02-%"
 GROUP BY p.product_id
 HAVING SUM(o.unit) >= 100

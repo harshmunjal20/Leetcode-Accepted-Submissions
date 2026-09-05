5286f91e7926451e20d97f3ed96b4605a3c88470
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-05 19:30:32 +0530

    Time: 274 ms (83.55%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,2 +1,16 @@
 # Write your MySQL query statement below
-SELECT (SELECT Salary FROM (SELECT DISTINCT salary , DENSE_RANK() OVER(ORDER BY Salary DESC) AS rnk FROM Employee) AS T WHERE T.rnk = 2) AS SecondHighestSalary
+-- WITH CTE AS (
+--     SELECT salary, 
+--     RANK() OVER(ORDER BY SALARY DESC) AS currRank
+--     FROM Employee
+-- )
+
+-- SELECT salary AS SecondHighestSalary 
+-- FROM CTE 
+-- WHERE currRank = 2
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (
+    SELECT MAX(Salary) FROM Employee
+)

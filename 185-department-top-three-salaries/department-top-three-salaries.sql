39d05c2367d375be306a598aafc434314ad5ec0a
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-05 13:05:54 +0530

    Time: 1170 ms (46.21%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,14 +1,22 @@
 # Write your MySQL query statement below
 # getting top 3 salaries
 
-WITH CTE AS (
+WITH ranked_salaries AS (
     SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary,
     DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS salary_rank
-    FROM Department d
-    INNER JOIN employee e
-    ON d.id = e.departmentId 
+    FROM Employee e
+    INNER JOIN Department d
+    ON e.departmentId = d.id
 )
 
 SELECT Department, Employee, Salary 
-FROM CTE 
+FROM ranked_salaries
 WHERE salary_rank <= 3
+
+-- top 3 in overall
+-- SELECT salary FROM (
+--     SELECT salary, id, 
+--     DENSE_RANK() OVER(ORDER BY Salary DESC) AS salary_rank
+--     FROM Department
+-- ) AS salary_ranks_table
+-- WHERE salary_rank <= 3

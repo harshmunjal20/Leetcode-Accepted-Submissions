ad879e688e78fec506d46b64e64db9a1bad4dd7a
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-06 14:02:51 +0530

    Time: 686 ms (67.27%) | Memory: 35.5 MB (50.94%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,38 +1,39 @@
 import heapq
-
 class MedianFinder(object):
 
     def __init__(self):
-        self.minHeap = []
         self.maxHeap = []
+        self.minHeap = []
 
     def addNum(self, num):
         """
         :type num: int
         :rtype: None
         """
-        if len(self.minHeap) >= len(self.maxHeap): # push into the maxHeap
-            if not self.maxHeap or -self.maxHeap[0] >= num:
-                heapq.heappush(self.maxHeap, -num)
-            else:
-                heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
-                heapq.heappush(self.minHeap, num)
-        else: # push into the minHeap
-            if num >= -self.maxHeap[0]:
-                heapq.heappush(self.minHeap, num)
-            else:
-                heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
-                heapq.heappush(self.maxHeap, -num)
+        # push into maxHeap , no matter what
+        heapq.heappush(self.maxHeap, -num)
+
+        # ensuring that top element in maxHeap is less than equal to minHeap top
+        if self.minHeap and self.maxHeap and (-self.maxHeap[0] > self.minHeap[0]):
+            heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+        
+        # maxHeap can have atmost 1 size more than minHeap
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
+        elif len(self.minHeap) > len(self.maxHeap):
+            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+
 
 
     def findMedian(self):
         """
         :rtype: float
         """
-        if (len(self.minHeap) + len(self.maxHeap)) % 2 != 0:
-            return min(-self.maxHeap[0] if self.maxHeap else 1e10, self.minHeap[0] if self.minHeap else 1e10)
+        if (len(self.maxHeap) + len(self.minHeap)) % 2 == 1:
+            return -self.maxHeap[0]
         
-        return (self.minHeap[0] - self.maxHeap[0]) / 2.00
+        return (self.minHeap[0] - self.maxHeap[0]) / 2.0
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:

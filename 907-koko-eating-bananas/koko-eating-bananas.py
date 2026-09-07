2bf71483d41daec69c82fe0d6c729963723828b4
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-07 08:18:44 +0530

    Time: 189 ms (33.48%) | Memory: 13.4 MB (50.13%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -6,28 +6,20 @@ class Solution(object):
         :rtype: int
         """
 
-        def hoursTaken(piles, bananas):
-            hoursTaken = 0
-
-            for currBananas in piles:
-                if currBananas <= bananas:
-                    hoursTaken += 1
-                else:
-                    hoursTaken += (currBananas / bananas) + (1 if (currBananas % bananas) != 0 else 0)
-
-            return hoursTaken
-
         left = 1
         right = max(piles)
-        minSpeed = 0
 
         while left <= right:
             mid = left + (right - left) / 2
 
-            if hoursTaken(piles, mid) <= h:
-                minSpeed = mid
+            currHours = 0
+
+            for bananas in piles:
+                currHours += (bananas / mid + (1 if bananas % mid != 0 else 0))
+
+            if currHours <= h:
                 right = mid - 1
             else:
                 left = mid + 1
 
-        return minSpeed
+        return left

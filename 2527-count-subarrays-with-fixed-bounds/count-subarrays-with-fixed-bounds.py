a8d3fd89dd4602c594b0c84f93a0afecde39bd03
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-08 18:22:53 +0530

    Time: 143 ms (83.87%) | Memory: 20.6 MB (48.39%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -6,20 +6,21 @@ class Solution(object):
         :type maxK: int
         :rtype: int
         """
-        lastMinIdx = -1
-        lastMaxIdx = -1
         prevInvalidIdx = -1
+        prevMinIdx = -1
+        prevMaxIdx = -1
         count = 0
 
         for idx in range(len(nums)):
             if nums[idx] == minK:
-                lastMinIdx = idx
+                prevMinIdx = idx
+            
             if nums[idx] == maxK:
-                lastMaxIdx = idx
-            elif minK > nums[idx] or nums[idx] > maxK:
+                prevMaxIdx = idx
+            elif nums[idx] > maxK or nums[idx] < minK:
                 prevInvalidIdx = idx
             
-            if lastMinIdx > prevInvalidIdx and lastMaxIdx > prevInvalidIdx:
-                count += min(lastMinIdx, lastMaxIdx) - prevInvalidIdx
-
+            if prevMinIdx > prevInvalidIdx and prevMaxIdx > prevInvalidIdx:
+                count += min(prevMinIdx, prevMaxIdx) - prevInvalidIdx
+        
         return count

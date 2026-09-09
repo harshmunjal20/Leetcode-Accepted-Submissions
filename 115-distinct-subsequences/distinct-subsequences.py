56fd8abf5d10abcd2859fe25dc129337efc3f368
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-09 17:08:23 +0530

    Time: 319 ms (71.31%) | Memory: 12.5 MB (50.91%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -8,16 +8,20 @@ class Solution(object):
         if len(s) < len(t):
             return 0
 
-        dp = [[0 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
+        dp = [0] * (len(t) + 1)
 
-        for idxS in range(len(s) + 1):
-            dp[idxS][len(t)] = 1
+        dp[len(t)] = 1
 
         for idxS in range(len(s) - 1 , -1, -1):
+            currDp = [0] * (len(t) + 1)
+            currDp[len(t)] = 1
+
             for idxT in range(len(t) - 1, -1, -1):
                 if s[idxS] == t[idxT]:
-                    dp[idxS][idxT] = dp[idxS + 1][idxT + 1] + dp[idxS + 1][idxT]
+                    currDp[idxT] = dp[idxT + 1] + dp[idxT]
                 else:
-                    dp[idxS][idxT] = dp[idxS + 1][idxT]
+                    currDp[idxT] = dp[idxT]
+            
+            dp = currDp
 
-        return dp[0][0]
+        return dp[0]

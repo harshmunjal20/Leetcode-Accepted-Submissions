6f1c8b3e4e780ac42aceb5da6efe0d76c351ff01
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-09 15:28:02 +0530

    Time: 2 ms (81.52%) | Memory: 12.4 MB (45.87%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -5,31 +5,31 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        def isValid(mid, nums):
-            students = 1
-            currSum = 0
-
-            for elem in nums:
-                currSum += elem
-
-                if elem > mid:
-                    return False
-                
-                if currSum > mid:
-                    students += 1
-                    currSum = elem
-
-            return students <= k
-
         low = 0
         high = sum(nums)
+
+        def isValid(nums, mid):
+            currStudent = 1
+            currSum = 0
+
+            for num in nums:
+                currSum += num
+
+                if num > mid:
+                    return False
+                
+                if currSum >  mid:
+                    currSum = num
+                    currStudent += 1
+
+            return currStudent <= k
 
         while low <= high:
             mid = low + (high - low) / 2
 
-            if isValid(mid, nums):
+            if isValid(nums, mid):
                 high = mid - 1
             else:
                 low = mid + 1
-        
+
         return low

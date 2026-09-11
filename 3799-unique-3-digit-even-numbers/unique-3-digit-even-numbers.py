a0acf559e1b675b2d34d6c4dfbb68339072334c4
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-11 18:23:49 +0530

    Time: 23 ms (75.40%) | Memory: 12.3 MB (92.86%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -5,21 +5,21 @@ class Solution(object):
         :rtype: int
         """
         used = [False] * 1000
-        totalDigits = len(digits)
         ans = 0
+        totalDigits = len(digits)
 
         for i in range(len(digits)):
             if digits[i] == 0:
                 continue
-
+            
             for j in range(len(digits)):
-                if i == j:
+                if j == i:
                     continue
 
                 for k in range(len(digits)):
                     if k == i or k == j or digits[k] % 2 != 0:
                         continue
-                    
+
                     number = digits[i] * 100 + digits[j] * 10 + digits[k]
                     
                     if not used[number]:

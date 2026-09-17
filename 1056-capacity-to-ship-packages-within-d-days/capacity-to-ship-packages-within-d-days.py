cebe6b7802989d5c0ee330923f100b46a23f8993
Harsh Munjal <sarlamunjal4@gmail.com>
2026-09-17 22:57:59 +0530

    Time: 203 ms (83.05%) | Memory: 24.2 MB (11.56%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shipWithinDays(self, weights: list[int], days: int) -> int:
+
+        def canShipPackages(mid) -> int:
+            numDays = 1
+            currSum = 0
+
+            for weight in weights:
+                currSum += weight
+
+                if weight > mid:
+                    return False
+
+                if currSum > mid:
+                    numDays += 1
+                    currSum = weight
+
+            return numDays <= days
+
+        low = 1
+        high = sum(weights)
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            print(mid)
+
+            if canShipPackages(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+            
+        return low
